5709bce5b4583662766090e7aba5d029d4840e6f
omikron15 <omikron15@hotmail.co.uk>
2018-06-09 21:40:43 +0100

    adds absic tests to customer spec file

--- specs/customer_spec.js
+++ specs/customer_spec.js
@@ -1,2 +1,24 @@
 const assert = require('assert')
 const Customer = require('../models/customer.js')
+
+describe("Customer", function(){
+
+  let customer1;
+
+  beforeEach(function(){
+    customer1 = new Customer("Store Name", "City 1");
+  });
+
+  it("has a name", function(){
+    assert.strictEqual(customer1.name, "Artist 1");
+  });
+
+  it("has a city", function(){
+    assert.strictEqual(customer1.city, "City 1");
+  });
+
+  it("has an inventory that starts empty", function(){
+    assert.strictEqual(customer1.inventory.length, 0);
+  });
+
+})
